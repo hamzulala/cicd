eeb7128fe91f2772421fd9bbc721d0786d7af64c
101852956 <101852956@TD224-021>
2019-12-02 10:17:54 +1100

    Added Unit Test

---
+++ CICDapp/RoomTests/UnitTest1.cs
@@ -0,0 +1,35 @@
+using ExampleWebApplication.Models;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Tests 
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void DefaultGetInfoTest()
+        {
+            Room r1 = new Room();
+            Assert.AreEqual("TD224 - 21", r1.getInfo());
+        }
+
+        [TestCase(315, "TB", 10)]
+        [TestCase(305, "TD", 25)]
+        [TestCase(315, "TB", 10)]
+        public void GetInfoTest(int n, string b, int c)
+        {
+            Room r1 = new Room(n, b, c);
+            Assert.AreEqual($"{b}{n} - {c}", r1.getInfo());
+        }
+    }
+}
